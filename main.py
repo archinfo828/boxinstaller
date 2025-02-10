aaf75d7b8970c7c23e2bb7a0633f2e89ffe22a0c
김명환 <142599292+archinfo828@users.noreply.github.com>
2025-02-10 15:25:11 +0900

    Annotate an Altair chart
    https://docs.streamlit.io/develop/tutorials/elements/annotate-an-altair-chart

--- main.py
+++ main.py
@@ -1,91 +1,50 @@
-import numpy as np
-import pandas as pd
 import streamlit as st
+import altair as alt
+import pandas as pd
+from vega_datasets import data
 
-from faker import Faker
 
 @st.cache_data
-def get_profile_dataset(number_of_items: int = 20, seed: int = 0) -> pd.DataFrame:
-    new_data = []
-
-    fake = Faker()
-    np.random.seed(seed)
-    Faker.seed(seed)
-
-    for i in range(number_of_items):
-        profile = fake.profile()
-        new_data.append(
-            {
-                "name": profile["name"],
-                "daily_activity": np.random.rand(25),
-                "activity": np.random.randint(2, 90, size=12),
-            }
-        )
-
-    profile_df = pd.DataFrame(new_data)
-    return profile_df
+def get_data():
+    source = data.stocks()
+    source = source[source.date.gt("2004-01-01")]
+    return source
 
 
-column_configuration = {
-    "name": st.column_config.TextColumn(
-        "Name", help="The name of the user", max_chars=100, width="medium"
-    ),
-    "activity": st.column_config.LineChartColumn(
-        "Activity (1 year)",
-        help="The user's activity over the last 1 year",
-        width="large",
-        y_min=0,
-        y_max=100,
-    ),
-    "daily_activity": st.column_config.BarChartColumn(
-        "Activity (daily)",
-        help="The user's activity in the last 25 days",
-        width="medium",
-        y_min=0,
-        y_max=1,
-    ),
-}
+stock_data = get_data()
 
-select, compare = st.tabs(["Select members", "Compare selected"])
+hover = alt.selection_single(
+    fields=["date"],
+    nearest=True,
+    on="mouseover",
+    empty="none",
+)
 
-with select:
-    st.header("All members")
-
-    df = get_profile_dataset()
-
-    event = st.dataframe(
-        df,
-        column_config=column_configuration,
-        use_container_width=True,
-        hide_index=True,
-        on_select="rerun",
-        selection_mode="multi-row",
+lines = (
+    alt.Chart(stock_data, title="Evolution of stock prices")
+    .mark_line()
+    .encode(
+        x="date",
+        y="price",
+        color="symbol",
     )
+)
 
-    st.header("Selected members")
-    people = event.selection.rows
-    filtered_df = df.iloc[people]
-    st.dataframe(
-        filtered_df,
-        column_config=column_configuration,
-        use_container_width=True,
+points = lines.transform_filter(hover).mark_circle(size=65)
+
+tooltips = (
+    alt.Chart(stock_data)
+    .mark_rule()
+    .encode(
+        x="yearmonthdate(date)",
+        y="price",
+        opacity=alt.condition(hover, alt.value(0.3), alt.value(0)),
+        tooltip=[
+            alt.Tooltip("date", title="Date"),
+            alt.Tooltip("price", title="Price (USD)"),
+        ],
     )
+    .add_selection(hover)
+)
 
-with compare:
-    activity_df = {}
-    for person in people:
-        activity_df[df.iloc[person]["name"]] = df.iloc[person]["activity"]
-    activity_df = pd.DataFrame(activity_df)
-
-    daily_activity_df = {}
-    for person in people:
-        daily_activity_df[df.iloc[person]["name"]] = df.iloc[person]["daily_activity"]
-    daily_activity_df = pd.DataFrame(daily_activity_df)
-
-    if len(people) > 0:
-        st.header("Daily activity comparison")
-        st.bar_chart(daily_activity_df)
-        st.header("Yearly activity comparison")
-        st.line_chart(activity_df)
-    else:
-        st.markdown("No members selected.")
+data_layer = lines + points + tooltips
